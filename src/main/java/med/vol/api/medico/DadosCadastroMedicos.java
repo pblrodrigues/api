b635b865ef49468efd7bd9d844fb800347d20083
Pablo Rodrigo Rodrigues <fulanodetal@exemplo.brpablo.rodrigues@dataprev.gov.br>
2023-06-01 19:17:12 -0300

    cadastrar medicos

---
+++ src/main/java/med/vol/api/medico/DadosCadastroMedicos.java
@@ -0,0 +1,30 @@
+package med.vol.api.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosCadastroMedicos(
+    @NotBlank
+    String nome, 
+    
+    @NotBlank
+    @Email
+    String email, 
+    
+    @NotBlank
+    String telefone,
+
+    @NotBlank
+    @Pattern(regexp = "\\d{4,6}")
+    String crm, 
+
+    @NotNull
+    Especialidade especialidade,
+
+    @NotNull
+    @Valid
+    DadosEndereco endereco) {
+}
